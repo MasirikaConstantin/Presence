b53ae28b1393d731bd40c8ba1ee7f729c19cd8c5
Masirika Constantin <elconstant73@gmail.com>
2024-11-28 21:55:15 +0100

    dropdown1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,10 @@
         <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/sort@3.x.x/dist/cdn.min.js"></script>
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
-
+        <script src="https://cdn.tailwindcss.com"></script>
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script src="https://cdn.tailwindcss.com"></script>
+        
         
     @else
    
