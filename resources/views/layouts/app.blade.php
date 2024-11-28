a58dad0c2f57a3f23f6358f8cb67dbf5d626636c
Masirika Constantin <elconstant73@gmail.com>
2024-11-28 22:00:57 +0100

    Font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,9 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Square+Peg&display=swap" rel="stylesheet">
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
@@ -27,6 +27,19 @@
    
 
     @endif
+
+    <style>
+        @font-face {
+                        font-family: 'Google';
+                        src: url('{{asset('ProductSans-Light.ttf')}}');
+                        font-weight: 500;
+                        
+                    }
+                    body{
+                        font-family: 'Google' !important;
+                    }
+                    
+    </style>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
