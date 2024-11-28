0b14543c0bf548302d823e09f4354607310f1d5c
Masirika Constantin <elconstant73@gmail.com>
2024-11-28 21:54:05 +0100

    dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,14 +15,14 @@
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
         <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
-   
+        <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/sort@3.x.x/dist/cdn.min.js"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    
 
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script src="https://cdn.tailwindcss.com"></script>
-        <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/sort@3.x.x/dist/cdn.min.js"></script>
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    
+        
     @else
    
 
